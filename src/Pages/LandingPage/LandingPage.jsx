0826a1b700d700838dc2db5515a9efe61dbd7936
Mise99 <Mise99>
2025-06-05 14:45:35 +0300

    my second add on twitter clone

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,29 +1,61 @@
-import React from 'react'
-import css from "./LandingPage.module.css"
-import logo from "../../images/sl_z_072523_61700_01.jpg"
-
+import React from "react";
+import css from "./LandingPage.module.css";
+import logo from "../../images/sl_z_072523_61700_01.jpg";
+import apple from "../../icons/apple.svg";
+import profile from "../../images/mitsuri-kanroji-3840x2160-16948.jpg"
+import google from "../../icons/google.svg"
 const LandingPage = () => {
   return (
-    <div className={css.frame}> 
-<div className={css.columns}>
+    <div className={css.frame}>
+      <div className={css.columns}>
+        {/*logo area */}
+        <div className={css.logo}>
+          <img src={logo} alt="" />
+        </div>
 
-    {/*logo area */}
-    <div className={css.logo}>
-<img src={logo} alt="" />
-    </div>
+        {/* content area */}
+        <div className={css.content}>
+          <h1>Happening now</h1>
+          <span>Join today.</span>
 
-    {/* content area */}
-    <div className={css.content}>
-         <h1>Happening now</h1>
-<span>
-   Join today.
-</span>
+          {/* Container 1 */}
+<div className={css.container1}>
+
+  {/* column one */}
+  <div className={css.columnone}>
+    <img src={profile} alt="my profile" />
+
+    {/* row one for text */}
+  <div className={css.rowone}>
+
+    <div className={css.username}>
+      <span>Sign in as Astrid</span>
     </div>
+  
+    <span>astrid25.atara@gmail.com</span>
+  </div>
+  </div>
+  <div className={css.column2}>
+    <img src={google} alt="my google icon" />
+  </div>
 </div>
 
-    </div>
-    
-  )
-}
+{/* Container 3 */}
+          <div className={css.container3}>
+            <img src={apple} alt="my aple logo" />
+            <span>Sign up with apple</span>
+          </div>
 
-export default LandingPage
+{/* container 2 */}
+<div className={css.container2}>
+  <span>Create account</span>
+</div>
+
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
